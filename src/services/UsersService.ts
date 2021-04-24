076c3753a94312f1a9ee3da2f88127eeff14ac84
William <w.willers@outlook.com>
2021-04-24 16:46:46 -0400

    Dia 03

---
+++ src/services/UsersService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+  async create(email: string) {
+    const userAlreadyExists = await this.usersRepository.findOne({ email });
+
+    if (userAlreadyExists) return userAlreadyExists;
+
+    const user = this.usersRepository.create({ email });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { UsersService };
